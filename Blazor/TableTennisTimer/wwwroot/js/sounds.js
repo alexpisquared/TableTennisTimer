d33dfd020c29265dd034840874b4b3a9e61c7579
alexpisqueared <alex.pigida@outlook.com>
2023-10-21 19:49:51 -0400

    PlayAudioFromCs

---
+++ Blazor/TableTennisTimer/wwwroot/js/sounds.js
@@ -0,0 +1,7 @@
+window.PlayAudio = (elementName) => {
+  document.getElementById(elementName).play();
+}
+
+window.PauseAudio = (elementName) => {
+  document.getElementById(elementName).pause();
+}
