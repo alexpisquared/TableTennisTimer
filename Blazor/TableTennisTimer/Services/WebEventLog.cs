e67967f81a54fcb849e5de1ba093d12c49888f5b
alexpisqueared <alex.pigida@outlook.com>
2023-11-12 18:47:55 -0500

    moved QR code to always visible at the bottom

--- Blazor/TableTennisTimer/Services/WebEventLog.cs
+++ Blazor/TableTennisTimer/Services/WebEventLog.cs
@@ -6,11 +6,11 @@ namespace TableTennisTimer.Services;
 public class WebEventLog
 {
   [NotMapped] public string[] Sub { get; set; }
-  [NotMapped] public string BrowserSignature { get; set; } = "Copy";
-  [NotMapped] public string Nickname { get; set; } = "Copy";      // don't change order!!! :parsing depends on it.
-  [NotMapped] public string FirstVisitId { get; set; } = "Copy";  // don't change order!!! :parsing depends on it.
+  [NotMapped] public string BrowserSignature { get; set; } = "New TTTimer";
+  [NotMapped] public string Nickname { get; set; } = "New TTTimer";      // don't change order!!! :parsing depends on it.
+  [NotMapped] public string FirstVisitId { get; set; } = "New TTTimer";  // don't change order!!! :parsing depends on it.
   public int Id { get; set; }
   public int WebsiteUserId { get; set; }
-  public string EventName { get; set; } = "Copy";
+  public string EventName { get; set; } = "New TTTimer";
   public DateTime DoneAt { get; set; }
 }
